4f7ffc508c657ffd38825035b9369a0be93b1987
Paweł Józefowski <dev@jzf.ski>
2022-12-09 14:14:39 +0100

    Update site description

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "🦖 JSaurus";
-export const SITE_DESCRIPTION = "Roar!";
+export const SITE_DESCRIPTION = "Just roaring but in JavaScript!";
